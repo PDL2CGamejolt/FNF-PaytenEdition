8a591eb24b39baeaed356da9e2f92a1fce803acb
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-10-27 19:21:57 -0500

    Lag less?
    Idk if I did it right.
    
    https://m.youtube.com/watch?v=s3shjhvwx7k&t=77s

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4101,6 +4101,7 @@ class PlayState extends MusicBeatState
 			pixelShitPart2 = '-pixel';
 		}
 
+                if(!cpuControlled) (
 		rating.loadGraphic(Paths.image(pixelShitPart1 + daRating.image + pixelShitPart2));
 		rating.cameras = [camHUD];
 		rating.screenCenter();
@@ -4224,6 +4225,7 @@ class PlayState extends MusicBeatState
 			startDelay: Conductor.crochet * 0.002
 		});
 	}
+        )
 
 	public var strumsBlocked:Array<Bool> = [];
 	private function onKeyPress(event:KeyboardEvent):Void
