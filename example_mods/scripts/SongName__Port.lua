a8ec85c41029feac8bd53832657bf7bd740bd3bd
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-11-09 08:42:12 -0600

    Update SongName__Port.lua

--- example_mods/scripts/SongName__Port.lua
+++ example_mods/scripts/SongName__Port.lua
@@ -1,5 +1,5 @@
 function onCreate()
-makeLuaText('watermark', songName .. " | Created By Uhard999 (test)", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
+makeLuaText('watermark', songName .. " | Payten Engine DEV BUILD", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
     setTextSize('watermark', 20);
     addLuaText('watermark');
 end
