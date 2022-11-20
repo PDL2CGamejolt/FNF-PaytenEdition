45a58cba38a5149a58b58d14fa70734242230e34
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-11-20 16:40:28 -0600

    Create Songthing.lua

---
+++ example_mods/scripts/Songthing.lua
@@ -0,0 +1,5 @@
+function onCreate()
+ makeLuaText('watermark', songName .. " | Payten Edition DEMO 1.0 | Credit to whoever owns the song", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
+     setTextSize('watermark', 20);
+     addLuaText('watermark');
+ end
