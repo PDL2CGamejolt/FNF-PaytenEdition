f292d055c4e61e11f8024ffebab5fa7151a2c63b
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-11-23 08:14:33 -0600

    Create deathcounts.lua

---
+++ example_mods/scripts/deathcounts.lua
@@ -0,0 +1,5 @@
+function onCreate()
+  makeLuaText('blue', "Blueballed: " + PlayState.deathCounter, 0, -5, 250); -- You can edit the created by Prisma Text just dont change anything else
+      setTextSize('blue', 20);
+      addLuaText('blue');
+  end
