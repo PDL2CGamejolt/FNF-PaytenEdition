75130c7b290132aa3c06aefbbc530d7db385aaec
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-11-29 18:54:08 -0600

    Create Universebreakermodchart.lua
    *snicker*

---
+++ Universebreakermodchart.lua
@@ -0,0 +1,42 @@
+//not finished yet
+
+//function onCreate()
+    setProperty("debugKeysChart", null)
+end
+
+//function onUpdate()
+    if keyboardJustPressed("SEVEN") then
+    debugPrint("OH NO YOU DON'T!, DEBUG MODE IS DISABLED"); 
+    playSound("missnote1");
+    end
+end
+
+//function onPause()
+    debugPrint("OH NO YOU DON'T!, PAUSING IS DISABLED"); 
+    playSound("missnote1");
+	return Function_Stop;
+end
+
+//function onCreate()     
+     if middlescroll then
+          setPropertyFromClass('ClientPrefs', 'middleScroll', false)
+     end
+end
+
+//function onDestroy() -- end of the song or you exited
+     if middlescroll then
+          setPropertyFromClass('ClientPrefs', 'middleScroll', true)
+     end
+end
+
+//function onCreate()     
+     if downscroll then
+          setPropertyFromClass('ClientPrefs', 'downScroll', false)
+     end
+end
+
+//function onDestroy() -- end of the song or you exited
+     if downscroll then
+          setPropertyFromClass('ClientPrefs', 'downScroll', true)
+     end
+end
