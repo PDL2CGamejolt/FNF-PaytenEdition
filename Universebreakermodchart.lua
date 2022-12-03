446955535e48d998b364cbe691407035c8250c11
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-12-03 15:30:22 -0600

    Update Universebreakermodchart.lua

--- Universebreakermodchart.lua
+++ Universebreakermodchart.lua
@@ -151,3 +151,17 @@ function opponentNoteHit()
 	end
 end
 
+function onUpdate(elapsed)
+
+  if curStep >= 0 then
+
+    songPos = getSongPosition()
+
+    local currentBeat = (songPos/1000)*(bpm/80)
+
+    doTweenY(dadTweenY, 'dad', 300-110*math.sin((currentBeat*0.25)*math.pi),0.001)
+
+  end
+
+end
+
