0113f89aafb40b634e288c823f6f2213e98c5392
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-12-03 18:36:45 -0600

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1191,8 +1191,10 @@ class PlayState extends MusicBeatState
 		scoreTxt.visible = !ClientPrefs.hideHud;
 		add(scoreTxt);
 
-		botplayTxt = new FlxText(400, timeBarBG.y + 55, FlxG.width - 800, "CHEATER", 32);
-		botplayTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.RED, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		botplayTxt = new FlxText(400, timeBarBG.y + 55, FlxG.width - 800, "Hey, I think you should get tested /n
+                because I think I might've given you /n
+                spamophobia...", 32);
+		botplayTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.YELLOW, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.RED);
 		botplayTxt.scrollFactor.set();
 		botplayTxt.borderSize = 1.25;
 		botplayTxt.visible = cpuControlled;
