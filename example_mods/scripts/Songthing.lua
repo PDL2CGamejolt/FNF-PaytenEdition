93ed5459981403c9fde86b47b267d96fd45dc27c
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-12-05 17:19:03 -0600

    Update Songthing.lua

--- example_mods/scripts/Songthing.lua
+++ example_mods/scripts/Songthing.lua
@@ -1,5 +1,14 @@
-function onCreate()
-  makeLuaText('watermark', songName .. " | Payten Edition DEMO 1.0 | Credit to everyone", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
-      setTextSize('watermark', 20);
-      addLuaText('watermark');
-  end
+function onCreatePost() --script made by impostor, credit me now or i will do an unfunny (credit to imposter dude)
+    --makeLuaText("message", "template", 500, 30, 50)
+    --setTextAlignment("message", "left")
+    --addLuaText("message")
+
+    makeLuaText("engineText", " songName ..  - Payten Edition Engine (PE "..version..")", 500, 30, 30)
+    setTextAlignment("engineText", "left")
+    addLuaText("engineText")
+
+    if getPropertyFromClass('ClientPrefs', 'downScroll') == false then
+        setProperty('message.y', 680)
+        setProperty('engineText.y', 660)
+    end
+end
