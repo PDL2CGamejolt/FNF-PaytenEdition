61ff9b16ecf27f445dfc0e8a90ba32c66948176c
Payten <94698464+PDL2CGamejolt@users.noreply.github.com>
2022-12-19 18:11:34 -0600

    Update script2.lua

--- script2.lua
+++ script2.lua
@@ -1021,3 +1021,22 @@ function onUpdate(elapsed)
 	-- start of "update", some variables weren't updated yet
     setTextString('healthText', 'Health: ' NO PEEKING)
 end
+
+local dead = false
+local balls = 0
+function onCreate()
+	makeLuaText('pog', 'Luck Counter: 0', 1300, 0, 0)
+	setTextAlignment('pog', 'center')
+	setTextSize('pog', 25, 25)
+	addLuaText('pog')
+end
+function onUpdate(elapsed)
+	if keyJustPressed('space') and getRandomBool(6) then
+		setProperty('health', 0)
+		dead = true
+	end
+	if keyJustPressed('space') and dead == false then
+		balls = balls + 1
+		setTextString('pog', 'Luck Counter: ' .. balls)
+	end
+end
